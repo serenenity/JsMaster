595120206914ee8916607a6da90043cd2f5a25f4
serenenity <mopaul@student.lautech.edu.ng>
2024-05-08 08:41:46 +0100

    array methods, marrio solution and objects

---
+++ solutions/marrio.js
@@ -0,0 +1,10 @@
+for(let i = 0; i < 8; i++){
+    for(let j = 8; j > 0; j--){
+        if( j > i){
+            console.log(" ".trim())
+        }else{
+            console.log("#".trim())
+        }
+    }
+    console.log()
+}
